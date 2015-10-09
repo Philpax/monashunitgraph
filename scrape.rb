d1fe297fe096f25cf89e3b980780a3eeab0cc2ea
Philpax <me@philpax.me>
2015-10-09 16:47:20 +1100

    Scrape all unit pages from start to target year, and store in separate folders

--- scrape.rb
+++ scrape.rb
@@ -9,9 +9,11 @@ YearTarget = 2016
 CourseCode = 4650
 
 Stem = 'http://www.monash.edu.au'
-UnitStartStem = "/pubs/#{YearStart}handbooks/units"
-UnitTargetStem = "/pubs/#{YearTarget}handbooks/units"
 UnitCodeRegex = /[A-Z]+[0-9]+/
+
+def get_unit_stem(year)
+	"/pubs/#{year}handbooks/units"
+end
 
 class Unit
 	attr_accessor :code
@@ -35,21 +37,27 @@ class Unit
 		@points = 0
 	end
 
-	def self.get(code)
-		url = "#{Stem}#{UnitTargetStem}/#{code}.html"
+	UnitCodeRegex = /([A-Z]{3,3}[0-9]+)/
+	def self.get(code, year)
+		url = "#{Stem}#{get_unit_stem(year)}/#{code}.html"
 		unit_page = Nokogiri::HTML(open(url))
 
 		unit = Unit.new code, url
 		unit.title = /: (.+) - 2/.match(unit_page.css('title').first.content)[1]
 
-		unit.prereqs = unit_page.css('div.uge-prerequisites-content').inner_html.scan(/([A-Z]{3,3}[0-9]+)/).flatten.uniq
-		unit.coreqs = unit_page.css('div.uge-co-requisites-content').inner_html.scan(/([A-Z]{3,3}[0-9]+)/).flatten.uniq
-		unit.prohibitions = unit_page.css('div.uge-prohibitions-content').inner_html.scan(/([A-Z]{3,3}[0-9]+)/).flatten.uniq
+		unit.prereqs = unit_page.css('div.uge-prerequisites-content').inner_html.scan(UnitCodeRegex).flatten.uniq
+		unit.coreqs = unit_page.css('div.uge-co-requisites-content').inner_html.scan(UnitCodeRegex).flatten.uniq
+		unit.prohibitions = unit_page.css('div.uge-prohibitions-content').inner_html.scan(UnitCodeRegex).flatten.uniq
  
 		unit.faculty = unit_page.css('p.pub_admin_faculty').first.content
 
-		offered = unit_page.css('ul.pub_highlight_value_offerings').at(0)
-		unit.offered = offered.children.map { |item| item.content } if offered
+		if year >= 2016 then
+			offered = unit_page.css('ul.pub_highlight_value_offerings').at(0)
+			unit.offered = offered.children.map { |item| item.content } if offered
+		else
+			offered = unit_page.css('a[href="index-bycampus-clayton.html"]')
+			unit.offered = offered.map { |e| e.next_sibling.text.strip }
+		end
 
 		unit.points = /([0-9]+)/.match(unit_page.css('div.content h2')[0])[1].to_i
 
@@ -71,55 +79,59 @@ class Unit
     end
 end
 
+def get_units_for_year(year, unit_codes)
+	units = {}
+
+	puts "=== #{year}"
+	unit_codes.each do |unit_code|
+		begin
+			units[unit_code] = unit = Unit.get(unit_code, year)
+			puts "#{unit.code}: #{unit.title}"
+		rescue OpenURI::HTTPError
+			puts "#{unit_code}: Doesn't exist"
+		end
+	end
+
+	puts "Getting additional units"
+	new_units = {}
+	units.each_value do |unit|
+		unit.prereqs.each do |prereq|
+			begin
+				if units[prereq].nil? && new_units[prereq].nil?
+					new_units[prereq] = unit = Unit.get(prereq, year)
+					puts "#{unit.code}: #{unit.title}"
+				end
+			rescue OpenURI::HTTPError
+				puts "#{prereq}: Doesn't exist"
+			end
+		end
+		unit.coreqs.each do |coreq|
+			begin
+				if units[coreq].nil? && new_units[coreq].nil?
+					new_units[coreq] = unit = Unit.get(coreq, year)
+					puts "#{unit.code}: #{unit.title}"
+				end
+			rescue OpenURI::HTTPError
+				puts "#{coreq}: Doesn't exist"
+			end
+		end
+	end
+
+	units.merge! new_units
+
+	path = File.join('units', year.to_s)
+	FileUtils.mkdir_p path
+	units.each do |key, value|
+		File.write(File.join(path, key + '.json'), value.to_json.to_s)
+	end
+
+	units
+end
+
 course_url = "#{Stem}/pubs/#{YearStart}handbooks/courses/#{CourseCode}.html"
 
 course_page = open course_url
-course_regex = /#{Regexp.quote(UnitStartStem)}\/[A-Z0-9]+.html/
+course_regex = /#{Regexp.quote(get_unit_stem(YearStart))}\/[A-Z0-9]+.html/
 
-unit_urls = course_page.read.scan(course_regex).uniq.map do |e| 
-	Stem + e.gsub("#{YearStart}handbooks", "#{YearTarget}handbooks")
-end
-
-units = {}
-
-unit_urls.each do |url|
-	unit_code = UnitCodeRegex.match(url).to_s 
-	begin
-		units[unit_code] = unit = Unit.get(unit_code)
-		puts "#{unit.code}: #{unit.title}"
-	rescue OpenURI::HTTPError
-		puts "#{unit_code}: Doesn't exist"
-	end
-end
-
-puts "Getting additional units"
-new_units = {}
-units.each_value do |unit|
-	unit.prereqs.each do |prereq|
-		begin
-			if units[prereq].nil? && new_units[prereq].nil?
-				new_units[prereq] = unit = Unit.get(prereq)
-				puts "#{unit.code}: #{unit.title}"
-			end
-		rescue OpenURI::HTTPError
-			puts "#{prereq}: Doesn't exist"
-		end
-	end
-	unit.coreqs.each do |coreq|
-		begin
-			if units[coreq].nil? && new_units[coreq].nil?
-				new_units[coreq] = unit = Unit.get(coreq)
-				puts "#{unit.code}: #{unit.title}"
-			end
-		rescue OpenURI::HTTPError
-			puts "#{coreq}: Doesn't exist"
-		end
-	end
-end
-
-units.merge! new_units
-
-FileUtils.mkdir_p 'units'
-units.each do |key, value|
-	File.write(File.join('units', key + '.json'), value.to_json.to_s)
-end
+unit_codes = course_page.read.scan(course_regex).uniq.map { |e| UnitCodeRegex.match(e).to_s }
+YearStart.upto(YearTarget) { |year| get_units_for_year(year, unit_codes) }
